947b75d9baa03838d6495116ad2a1e670ffcbacf
Florian <florian.schwanz@interoberlin.de>
2016-08-23 00:30:22 +0200

    Added enum names

--- lib/src/main/java/de/interoberlin/merlot_android/model/mapping/actions/EActionType.java
+++ lib/src/main/java/de/interoberlin/merlot_android/model/mapping/actions/EActionType.java
@@ -1,14 +1,18 @@
 package de.interoberlin.merlot_android.model.mapping.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum EActionType {
     // <editor-fold defaultstate="collapsed" desc="Entries">
 
-    WRITE_CHARACTERISTIC(WriteCharacteristicAction.class);
+    WRITE_CHARACTERISTIC("write characteristic", WriteCharacteristicAction.class);
 
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Members">
 
+    private final String name;
     private final Class c;
 
     // </editor-fold>
@@ -19,17 +23,38 @@ public enum EActionType {
 
     // <editor-fold defaultstate="collapsed" desc="Constructors">
 
-    EActionType(Class c) {
+    EActionType(String name, Class c) {
+        this.name = name;
         this.c = c;
     }
 
     // </editor-fold>
 
     // --------------------
+    // Methods
+    // --------------------
+
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+
+    public static List<String> getNamesList() {
+        List names = new ArrayList<>();
+
+        for (EActionType e : EActionType.values()) {
+            names.add(e.getName());
+        }
+
+        return names;
+    }
+
+    // --------------------
     // Getters / Setters
     // --------------------
 
     // <editor-fold defaultstate="collapsed" desc="Getters / Setters">
+
+    public String getName() {
+        return name;
+    }
 
     public Class getC() {
         return c;
--- lib/src/main/java/de/interoberlin/merlot_android/model/mapping/functions/EFunctionType.java
+++ lib/src/main/java/de/interoberlin/merlot_android/model/mapping/functions/EFunctionType.java
@@ -1,14 +1,18 @@
 package de.interoberlin.merlot_android.model.mapping.functions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum EFunctionType {
     // <editor-fold defaultstate="collapsed" desc="Entries">
 
-    THRESHOLD(ThresholdFunction.class);
+    THRESHOLD("threshold", ThresholdFunction.class);
 
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Members">
 
+    private final String name;
     private final Class c;
 
     // </editor-fold>
@@ -19,8 +23,27 @@ public enum EFunctionType {
 
     // <editor-fold defaultstate="collapsed" desc="Constructors">
 
-    EFunctionType(Class c) {
+    EFunctionType(String name, Class c) {
+        this.name = name;
         this.c = c;
+    }
+
+    // </editor-fold>
+
+    // --------------------
+    // Methods
+    // --------------------
+
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+
+    public static List<String> getNamesList() {
+        List names = new ArrayList<>();
+
+        for (EFunctionType e : EFunctionType.values()) {
+            names.add(e.getName());
+        }
+
+        return names;
     }
 
     // </editor-fold>
@@ -30,6 +53,11 @@ public enum EFunctionType {
     // --------------------
 
     // <editor-fold defaultstate="collapsed" desc="Getters / Setters">
+
+    public String getName() {
+        return name;
+    }
+
 
     public Class getC() {
         return c;
